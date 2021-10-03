7aa90025e7fb2f46cfb6fb03a1b2049e9e1c298a
Matthew E. Grahlman <grahlmanmatthew@gmail.com>
2021-10-03 01:34:13 -0400

    Added AABB Tree collision detection for word boxes

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import sys
-import math
 import random
 from PIL import Image, ImageFont, ImageDraw
 from wordcloud.stopwords import set_stopwords
 from wordcloud.vocabulary import create_vocab
+from aabbtree import AABB, AABBTree
 
 # WORD CLOUD INPUT PARAMS
 NUM_WORDS = 25
+CONST_SCALE = 12
 FONT_SIZE_MULTIPLIER = 7
 IMAGE_WIDTH, IMAGE_HEIGHT = 500, 500
 FONT_COLOUR = (0, 0, 0) # black
 STOPWORDS_DATA_PATH = './lang/stopwords.txt'
 stopwords = set_stopwords(STOPWORDS_DATA_PATH)
-
-WX_FONT = "./fonts/PT_Sans-Regular.ttf"
 
 # NORMALIZE INPUT DATA FROM FILE AND BUILD WORD DICT
 INPUT_DATA_PATH = './input/data.txt'
@@ -38,6 +37,7 @@ for word in vocab_sizes:
     vocab_sizes[word] = int(font_size)
 
 # DRAW WORDS
+tree = AABBTree()
 while len(vocab) > 0:
     # info about the word
     word_tuple = vocab.popitem()
@@ -50,16 +50,25 @@ while len(vocab) > 0:
     while not valid:
         # Places each word on the canvas, draws a rectangle around it
         coords = (random.randint(0,500), random.randint(0,500))
-        CONST_SCALE = 12
-        word_width = font.getsize(word)
-        word_box_start = (coords[0] - CONST_SCALE, coords[1])
-        word_box_end = (coords[0] + word_width[0] + CONST_SCALE , coords[1] + word_width[1]) 
-        if word_box_start[0] < IMAGE_WIDTH and word_box_end[0] < IMAGE_WIDTH:
-            if word_box_start[1] < IMAGE_HEIGHT and word_box_end[1] < IMAGE_HEIGHT:
-                valid = True
-    draw.rectangle((word_box_start, word_box_end), outline=(255,0,0), width=1)
-    draw.text(coords, word, FONT_COLOUR, font=font)
+        word_box_size = font.getsize(word)
 
+        # Checks whether the box surrounding thwe word is within the bounds of the canvas
+        word_box = (coords[0] - CONST_SCALE, coords[1]), (coords[0] + word_box_size[0] + CONST_SCALE, coords[1] + word_box_size[1])
+        if word_box[0][0] < IMAGE_WIDTH and word_box[1][0] < IMAGE_WIDTH:
+            if word_box[0][1] < IMAGE_HEIGHT and word_box[1][1] < IMAGE_HEIGHT:
+                valid = True
+
+        # AABB (Axis Aligned Bounding Box) Tree to determine whether word box collides with a word that's already been placed.
+        limits = [(word_box[0][0], word_box[1][0]), (word_box[0][1], word_box[1][1])]
+        aabb = AABB(limits)
+        if tree.does_overlap(aabb):
+            valid = False
+
+    tree.add(aabb, word)
+
+    draw.rectangle(word_box, outline=(255,0,0), width=1)
+    draw.text(coords, word, FONT_COLOUR, font=font)
+    #sys.exit()
 
 img.save(IMG_FILEPATH)
 
