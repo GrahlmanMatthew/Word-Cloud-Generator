9a3cf68c7e336306629f7f06145dda099dcf33d9
Matthew E. Grahlman <grahlmanmatthew@gmail.com>
2021-10-04 21:09:15 -0400

    Fixed edge case where word cloud would never finish generating

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ from wordcloud.stopwords import set_stopwords
 from wordcloud.vocabulary import create_vocab
 from wordcloud.grid import GridGenerator, center_word_in_square
 from wordcloud.cloud import place_words
-from aabbtree import AABB, AABBTree
 
 # INPUT DATA FILE PATHS
 STOPWORDS_DATA_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '.', 'lang', 'stopwords.txt'))
 INPUT_DATA_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '.', 'input', 'data.txt'))
 
 # WORD CLOUD SETTINGS FOR OUTPUT IMAGE
-NUM_WORDS = 32
+NUM_WORDS = 16
 IMAGE_WIDTH = 500
 IMAGE_HEIGHT = 500
 
@@ -19,23 +18,10 @@ IMAGE_HEIGHT = 500
 # 3. MAKE WORD PLACEMENT FAVOUR THE CENTER OF THE CANVAS
 # 4. CLEAN UP IMAGE GENERATION [wip]
 CONST_SCALE = 12
-FONT_SIZE_MULTIPLIER = 7
 
 # NORMALIZE INPUT DATA FROM FILE AND BUILD WORD DICT
 stopwords = set_stopwords(STOPWORDS_DATA_PATH)
 vocab = create_vocab(NUM_WORDS, INPUT_DATA_PATH, stopwords)
-
-# DETERMINE FONT SIZES - FIRST NEED TO GET TOTAL NUMBER OF OCCURENCES OF ALL WORDS IN OUR VOCAB
-vocab_sizes = {}
-num_vocab_occurences = 0
-for word in vocab:
-    vocab_sizes[word] = vocab[word]
-    num_vocab_occurences += vocab[word]
-
-# DETERMINE FONT SIZE FOR EACH WORD BASED ON FONT_SIZE_MULTIPLIER
-for word in vocab_sizes:
-    font_size = round((vocab_sizes[word] * FONT_SIZE_MULTIPLIER / num_vocab_occurences) * 100, 0)
-    vocab_sizes[word] = int(font_size)
 
 # Generates grid of grids and coords of potential positions to place words on these grids
 grid_gen = GridGenerator(IMAGE_WIDTH, IMAGE_HEIGHT, NUM_WORDS)
@@ -45,4 +31,4 @@ grid = grid_gen.get_grid()
 # CREATES WORDLCOUD
 WC_IMG_FILEPATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'output', 'wordcloud.png'))   
 WC_BOUNDS_IMG_FILEPATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'output', 'word-bounds.png'))
-place_words(vocab, vocab_sizes, grid, grid_gen)
+place_words(vocab, grid, grid_gen)
--- wordcloud/cloud.py
+++ wordcloud/cloud.py
@@ -1,4 +1,6 @@
+import sys
 import os
+import math
 import random
 from copy import deepcopy
 from aabbtree import AABB, AABBTree
@@ -10,9 +12,10 @@ WC_IMG_FILEPATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'
 WC_BOUNDS_IMG_FILEPATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'output', 'word-bounds.png'))  
 
 # Place words in the vocabulary on the grid
-def place_words(vocab, vocab_sizes, grid, grid_gen, wc_img_path=WC_IMG_FILEPATH, wc_bounds_img_path=WC_BOUNDS_IMG_FILEPATH):
+def place_words(vocab, grid, grid_gen, wc_img_path=WC_IMG_FILEPATH, wc_bounds_img_path=WC_BOUNDS_IMG_FILEPATH):
     voc = deepcopy(vocab)
-    voc_sizes = deepcopy(vocab_sizes)
+    voc_size = get_all_vocab_occurences(voc)   # calc num occurences of each word
+    font_sizes = calculate_font_sizes(vocab, voc_size)
     grd = deepcopy(grid)
     grd_width = grid_gen.get_grid_width()
     grd_height = grid_gen.get_grid_height()
@@ -30,7 +33,7 @@ def place_words(vocab, vocab_sizes, grid, grid_gen, wc_img_path=WC_IMG_FILEPATH,
         # info about the word to be placed
         word_tuple = voc.popitem()
         word = word_tuple[0]
-        word_size = voc_sizes[word]
+        word_size = font_sizes[word]
         font = ImageFont.truetype(get_random_font_path(), word_size)
 
         # find valid placement for this word in the grid
@@ -44,21 +47,53 @@ def place_words(vocab, vocab_sizes, grid, grid_gen, wc_img_path=WC_IMG_FILEPATH,
                 aabb = AABB([(word_box[0][0], word_box[1][0]), (word_box[0][1], word_box[1][1])])   # potential node (word placement) in tree
                 collides = word_box_tree.does_overlap(aabb)     # returns true if node (word placement) does not collide with already placed words
 
+                print(word, word_size, word_box_size)
+
+
                 if not collides:
                     if word_box[0][0] > 0 and word_box[1][0] < grd_width and word_box[0][1] > 0 and word_box[1][1] < grd_height:
                         valid_placement = True
                         grd.remove(square)      # removes square from grid so we can't place another word there
                         word_box_tree.add(aabb, word)
                         break  
+            
+            if not valid_placement:
+                break
+
 
         WC_BOUNDS_DRAW.rectangle(word_box, outline=(255,0,0), width=1)
-        WC_DRAW.text(coords, word, (0, 0, 0), font=font)
-        
+        WC_DRAW.text(coords, word, (0, 0, 0), font=font)  
     WC_BOUNDS_IMG.save(wc_bounds_img_path)
     WC_IMG.save(wc_img_path)
 
+# RANDOMLY SELECT RANDOM FONT PATH
 def get_random_font_path():
     FONT_DIR = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'fonts'))
     font_name = random.choice(os.listdir(FONT_DIR))
     font_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', FONT_DIR, font_name))
+    print(font_path)
     return font_path
+
+# DETERMINE FONT SIZES - FIRST NEED TO GET TOTAL NUMBER OF OCCURENCES OF ALL WORDS IN OUR VOCAB
+def get_all_vocab_occurences(vocab):
+    vocab_sizes = {}
+    num_vocab_occurences = 0
+    for word in vocab:
+        vocab_sizes[word] = vocab[word]
+        num_vocab_occurences += vocab[word]
+    return num_vocab_occurences
+
+# CALCULATE FONT SIZE FOR EACH WORD
+def calculate_font_sizes(vocab, voc_size):
+    vocab_sizes = {}
+
+    print(voc_size)
+    FONT_SIZE_MULTIPLIER = 7
+    FONT_SIZE_MULTIPLIER = math.floor(math.log(voc_size, 10) * 2)
+    print(FONT_SIZE_MULTIPLIER)
+    
+    for word in vocab:
+        font_size = round((vocab[word] * FONT_SIZE_MULTIPLIER / voc_size) * 100, 0)
+        print(font_size)
+        vocab_sizes[word] = int(font_size)
+    return vocab_sizes
