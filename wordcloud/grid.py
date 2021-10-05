9b5c0ba8b27617ac9ec25bfbef1e533ee3cc05c1
Matthew E. Grahlman <grahlmanmatthew@gmail.com>
2021-10-04 20:04:57 -0400

    Added opt param for file output location

--- wordcloud/grid.py
+++ wordcloud/grid.py
@@ -1,9 +1,11 @@
-from PIL import Image, ImageFont, ImageDraw
+import os
+from PIL import Image, ImageDraw
+
+# DEFAULT OUTPUT PATH
+GRID_IMG_FILEPATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'output', 'word-placement-grid.png'))
 
 # GRID GENERATOR
 class GridGenerator:
-   # square_list = []
-
     def __init__(self, image_width, image_height, num_words):
         self.grid_width = image_width
         self.grid_height = image_height
@@ -15,8 +17,7 @@ class GridGenerator:
 
         self.grid = generate_grid_coords(self.grid_width, self.grid_height, self.num_squares)
 
-    def draw_grid(self):
-        GRID_FILEPATH = './output/word-placement-grid.png'
+    def draw_grid(self, grid_path=GRID_IMG_FILEPATH):
         GRID_IMG = Image.new('RGB', (self.grid_width, self.grid_height), color='white')
         draw = ImageDraw.Draw(GRID_IMG)
 
@@ -31,26 +32,16 @@ class GridGenerator:
             else:
                 draw.rectangle(square, outline=(255,0,255), width = 1)
             count += 1
-
-        GRID_IMG.save(GRID_FILEPATH)
-
-        # IMG2_FILEPATH = './output/word-placement-grid.png'
-        # img2 = Image.new('RGB', (IMAGE_WIDTH, IMAGE_HEIGHT), color='white')
-        # draw2 = ImageDraw.Draw(img2)
-        # count = 1
-        # for sq in grid:
-        #     if count <= pow(4, 0):
-        #         draw.rectangle(sq, outline=(255,0,0), width = 12)
-        #     elif count <= pow(4, 1) + pow(4, 0):
-        #         draw.rectangle(sq, outline=(0,255,0), width = 6)
-        #     elif count <= pow(4, 2) + pow(4, 1) + pow(4, 0):
-        #         draw.rectangle(sq, outline=(0,0,255), width = 3)
-        #     else:
-        #         draw.rectangle(sq, outline=(255,0,255), width = 1)
-        #     count += 1
+        GRID_IMG.save(grid_path)
 
     def get_grid(self):
         return self.grid
+    
+    def get_grid_width(self):
+        return self.grid_width
+
+    def get_grid_height(self):
+        return self.grid_height
 
 # Find 4 subsquares within a given square, just provide start/end coords for the initial square
 def find_subsquares(start_coord, end_coord):
@@ -76,15 +67,9 @@ def find_subsquares(start_coord, end_coord):
     return (sq1, sq2, sq3, sq4)
 
 def center_word_in_square(square, word_box):
-    width = square[1][0] - square[0][0]
-    height = square[1][1] - square[0][1]
-
-    word_width = word_box[0]
-    word_height = word_box[1]
-
-    new_width = ((width - word_width)/2) + square[0][0]
-    new_height = ((height - word_height)/2) + square[0][1]
-
+    width, height = square[1][0] - square[0][0], square[1][1] - square[0][1]
+    word_width, word_height = word_box[0], word_box[1]
+    new_width, new_height = ((width - word_width)/2) + square[0][0], ((height - word_height)/2) + square[0][1]
     return (new_width, new_height)
 
 def calculate_num_squares(num_words):
